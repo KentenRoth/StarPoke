086ecb2738f07841606f492d23efc80df2d81970
Kent Roth <kentencroth@yahoo.com>
2019-01-27 15:51:51 -0600

    WIP: Added api folders and api files

--- starpoke/src/components/App.js
+++ starpoke/src/components/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import starwars from "../apis/starwars";
+import pokemon from "../apis/pokemon";
 import Header from "./Header";
 
 class App extends Component {
