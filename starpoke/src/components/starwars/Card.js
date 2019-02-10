465ac334d86ed5e83060b668f1079ce7842ad1b3
KentenRoth <kentencroth@yahoo.com>
2019-02-10 16:29:15 -0600

    WIP: Setting up Star Wars card

--- starpoke/src/components/starwars/Card.js
+++ starpoke/src/components/starwars/Card.js
@@ -1,10 +1,22 @@
 import React from "react";
+import "./Card.css";
 
 const Card = props => {
+    const { name, height, mass, hair_color, eye_color } = props.baseData;
     return (
-        <div>
-            {props.baseData}
-            {props.starWars}
+        <div className="card">
+            <div className="cardName">
+                {name}
+                <br />
+                Height: {height} cm
+                <br />
+                Weight: {mass} kg
+                <br />
+                Eye Color: {eye_color}
+                <br />
+                Hair Color: {hair_color}
+                {props.starWars}
+            </div>
         </div>
     );
 };
--- starpoke/src/components/starwars/StarCardList.js
+++ starpoke/src/components/starwars/StarCardList.js
@@ -3,11 +3,11 @@ import Card from "./Card";
 
 const StarCardList = props => {
     const searchResults = props.responseResults.map(starWars => {
-        return <Card key={starWars.name} starWars={starWars.name} />;
+        return <Card key={starWars.name} starWars={starWars} />;
     });
 
     const baseData = props.starWarsData.map(baseData => {
-        return <Card key={baseData.name} baseData={baseData.name} />;
+        return <Card key={baseData.name} baseData={baseData} />;
     });
 
     if (props.responseResults.length === 0) {
