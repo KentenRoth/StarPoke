2a1ea997c8fb8da77de2ad73267d95af27e07ee4
Kent Roth <kentencroth@yahoo.com>
2019-02-12 19:50:07 -0600

    WIP: setup pokemon list

---
+++ starpoke/src/components/pokemon/BaseCard.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Card.css";
+
+const BaseCard = () => {
+    return <div>Card</div>;
+};
+
+export default BaseCard;
--- starpoke/src/components/pokemon/PokeCardList.js
+++ starpoke/src/components/pokemon/PokeCardList.js
@@ -1,9 +1,29 @@
-import React, { Component } from "react";
+import React from "react";
+import SearchCard from "./SearchCard";
+import BaseCard from "./BaseCard";
 
-class PokeCardList extends Component {
-    render() {
-        return <div>PokeCardList</div>;
+const PokeCardList = props => {
+    const searchResults = props.responseResults.map(poke => {
+        return <SearchCard key={poke.name} poke={poke} />;
+    });
+
+    const baseData = props.pokemonData.map(baseData => {
+        return <BaseCard key={baseData.name} baseData={baseData} />;
+    });
+
+    if (props.responseResults.length === 0) {
+        return (
+            <div className="container">
+                <div>{baseData}</div>
+            </div>
+        );
+    } else {
+        return (
+            <div className="container">
+                <div>{searchResults}</div>
+            </div>
+        );
     }
-}
+};
 
 export default PokeCardList;
