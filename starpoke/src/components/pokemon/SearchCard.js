eabdb504456b6a5c9bff0fdbb60b1e18b15e7e71
Kent Roth <kentencroth@yahoo.com>
2019-02-13 19:12:02 -0600

    WIP: Getting the data into the search results card

--- starpoke/src/components/pokemon/SearchCard.js
+++ starpoke/src/components/pokemon/SearchCard.js
@@ -1,7 +1,23 @@
 import React from "react";
 
 const SearchCard = props => {
-    return <div>Search Card</div>;
+    return (
+        <div>
+            <img src={props.poke.sprites.front_shiny} />
+            <br />
+            Name: {props.poke.name}
+            <br />
+            Base Experience: {props.poke.base_experience}
+            <br />
+            Abilities
+            <br />
+            {props.poke.abilities[0].ability.name}
+            <br />
+            {props.poke.abilities[1].ability.name}
+            <br />
+            <img src={props.poke.sprites.back_shiny} />
+        </div>
+    );
 };
 
 export default SearchCard;
