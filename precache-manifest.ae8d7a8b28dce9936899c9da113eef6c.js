daca458da2c0b056a4c23fe7579fb4ac212a3f17
KentenRoth <kentencroth@yahoo.com>
2019-02-26 12:50:21 -0600

    Updates

---
+++ precache-manifest.ae8d7a8b28dce9936899c9da113eef6c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9e92fef0b9edad392ced",
+    "url": "/StarPoke/static/js/runtime~main.9e92fef0.js"
+  },
+  {
+    "revision": "855ceb87f1c1a0990bf9",
+    "url": "/StarPoke/static/js/main.855ceb87.chunk.js"
+  },
+  {
+    "revision": "f9da11cef970980948b5",
+    "url": "/StarPoke/static/js/2.f9da11ce.chunk.js"
+  },
+  {
+    "revision": "855ceb87f1c1a0990bf9",
+    "url": "/StarPoke/static/css/main.5efbf8d9.chunk.css"
+  },
+  {
+    "revision": "3bf0686df5412fbb1d9d187d2e47b689",
+    "url": "/StarPoke/index.html"
+  }
+];
